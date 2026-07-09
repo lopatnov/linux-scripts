b1ce3515b6290186c7a8cfb5a9a529ddb29f8f4d
Oleksandr Lopatnov <oleksandr@lopatnov.cv.ua>
2026-07-09 17:50:21 +0300

    Changed port to 3390

--- wsl/debian/install-lxqt.sh
+++ wsl/debian/install-lxqt.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
-# Установка LXQt на Debian 13 WSL. Как видно из пакетов - без дополнительного софта
+# Установка LXQt Core на Debian 13 WSL. Подключение через RDP.
 
 sudo apt update
 sudo apt install -y lxqt-core xrdp dbus-x11
+
+# ХИТРЫЙ ШАГ: Меняем стандартный порт 3389 на 3390, чтобы избежать конфликта с Windows
+sudo sed -i 's/port=3389/port=3390/g' /etc/xrdp/xrdp.ini
+
+# Настраиваем запуск LXQt для текущего пользователя
 echo "startlxqt" > ~/.xsession
+
+# Добавляем права для корректной работы SSL в xrdp
 sudo adduser xrdp ssl-cert
